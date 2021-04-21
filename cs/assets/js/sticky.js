d20d688e6ebb6ae100479729831fd45f3323c4b9
Hüseyin Yürekli <20050634@ogrenci.ankara.edu.tr>
2021-04-22 02:37:33 +0300

    section header v1

--- cs/assets/js/sticky.js
+++ cs/assets/js/sticky.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction()
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
